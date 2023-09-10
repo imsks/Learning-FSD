e85752630be36e372a2f46d0dacbc02881137dd1
Sachin <sachinkshuklaoo7@gmail.com>
2023-09-10 19:26:30 +0530

    Added JS

---
+++ intro-to-javascript/index.js
@@ -0,0 +1,23 @@
+// console.log("Your first Javascript File") // Used to print information
+
+// Variable
+var x = 20
+// console.log(x)
+
+var y = true
+
+// Condition
+var isStudent = false
+
+// if (5 > 3) {
+//     console.log("Student exist")
+// } else if (5 > 8) {
+//     console.log("Student exist")
+// } else {
+//     console.log("Student not exist")
+// }
+
+// Loop
+for (var i = 0; i < 10; i++) {
+    console.log(i)
+}
